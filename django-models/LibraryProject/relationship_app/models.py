af3939710e3eed3794e82cee6c92cdb9c9acc1cb
Nare33 <ulendaphaho@gmail.com>
2025-02-20 06:16:51 -0800

    URL config

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,22 +8,18 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
 
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
 
