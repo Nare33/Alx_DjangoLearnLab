13160f0295c5e51b4c5ca6aa42d20c136452bdd6
Nare33 <ulendaphaho@gmail.com>
2025-02-22 08:31:39 -0800

    Control in django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,17 +1,10 @@
+import datetime 
 from django.db import models
-import datetime
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    publication_date = models.DateField(default=datetime.date(2025, 2 ,2))
-
-    class Meta:
-        permissions = [
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
-        ]
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField(default=datetime.date(2025, 2, 2))
 
     def __str__(self):
         return self.title
