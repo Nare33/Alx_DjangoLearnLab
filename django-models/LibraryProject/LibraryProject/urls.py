45cbf669811cc99665ac5ef9fa101631952f08a9
Nare33 <ulendaphaho@gmail.com>
2025-02-24 00:25:58 -0800

    URL config

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('relationship_app.urls')),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,48 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login,authenticate
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            user = authenticate(username=username, password=form.cleaned_data.get('password1'))
-            login(request, user)
-            return redirect('home')
+            messages.success(request, 'Your account has been created!')
+            return redirect('login')  
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
-class CustomLogoutView(LogoutView):
-    template_name = 'logout.html'
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
 
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
