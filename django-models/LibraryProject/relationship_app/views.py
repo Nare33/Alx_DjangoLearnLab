d53a74e8c76e01ecf845f854a8fc5575dd55ad8c
Nare33 <ulendaphaho@gmail.com>
2025-02-25 04:22:41 -0800

    Permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,7 +37,7 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')  # Redirect after deleting
+        return redirect('book_list')  
     return render(request, 'delete_book.html', {'book': book})
 
 
