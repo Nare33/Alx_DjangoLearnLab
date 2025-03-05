50aa076c8337f9970c4f627a35b8ed03bc5dd473
Nare33 <ulendaphaho@gmail.com>
2025-03-05 04:51:40 -0800

    ViewSets and routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', include(router.urls)),
 ]
