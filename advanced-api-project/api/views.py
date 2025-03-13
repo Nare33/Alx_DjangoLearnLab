9a223ad07de2e73236205e315bf89796e4055ccf
Nare33 <ulendaphaho@gmail.com>
2025-03-13 07:52:10 -0700

    Unit test

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,11 @@
-# views.py
-from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-from .filters import BookFilter  # If using custom filtering
+from .filters import BookFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
-class BookListView(generics.ListCreateAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
-    
-    filterset_class = BookFilter  # Optional if using a custom filter set
-    
-    search_fields = ['title', 'author']
-    ordering_fields = ['title', 'publication_year']
-    ordering = ['title']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
