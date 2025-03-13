30126d7b61207c5066d6fad291c9cb13855df890
Nare33 <ulendaphaho@gmail.com>
2025-03-13 05:40:26 -0700

    Filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,11 +2,27 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, filters
+from rest_framework import generics, filters as drf_filters
+
+class BookFilter(FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')  # Case-insensitive filtering
+    author = filters.CharFilter(lookup_expr='icontains')  # Case-insensitive filtering
+    publication_year = filters.NumberFilter()
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
+     filter_backends = (DjangoFilterBackend, drf_filters.SearchFilter, drf_filters.OrderingFilter)
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
