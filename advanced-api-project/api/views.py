53fed2fb56c42f3edb21e0f82c7b598121da80fc
Nare33 <ulendaphaho@gmail.com>
2025-03-13 23:37:50 -0700

    Filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from .filters import BookFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters import FilterSet, CharFilter, NumberFilter
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookFilter(FilterSet):
+    title = CharFilter(lookup_expr='icontains')  
+    author = CharFilter(lookup_expr='icontains')
+    publication_year = NumberFilter(field_name='publication_date', lookup_expr='year')
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_date']  
+    ordering = ['title']  
+
