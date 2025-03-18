2eb44a380876df1518246a6daf102cb949c98696
Nare33 <ulendaphaho@gmail.com>
2025-03-18 04:04:12 -0700

    User functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -32,16 +32,11 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    success_url = reverse_lazy('post-list')
 
     def test_func(self):
         post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        return self.request.user == post.author
 
 # Delete view to allow users to delete their posts
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
