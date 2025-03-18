9b9097c8ac4d71fffd6f2e167fd01e292bb7e8ab
Nare33 <ulendaphaho@gmail.com>
2025-03-18 00:50:58 -0700

    User authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        password_form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+            return redirect('profile')
+        elif password_form.is_valid():
+            user = password_form.save()
+            update_session_auth_hash(request,user)
+            return redirect('profile')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+        password_form = PasswordChangeForm(request.user)
+    return render(request, 'registration/profile.html', {'form': form, "password_form": password_form})
+
