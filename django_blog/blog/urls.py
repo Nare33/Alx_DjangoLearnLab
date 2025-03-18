4ad0ab40d0bb5f63c58b096828d966e988cadac1
Nare33 <ulendaphaho@gmail.com>
2025-03-18 06:10:37 -0700

    Comment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -7,8 +8,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
-    path('posts/<int:pk>/comments/new/', views.comment_new, name='comment_new'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),  
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),  
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),  
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,13 +12,13 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/post_detail.html'
 
     def form_valid(self, form):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
         form.instance.author = self.request.user
         form.instance.post = post
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'post_id': self.kwargs['post_id']})
+        return reverse_lazy('post_detail', kwargs={'post_id': self.kwargs['pk']})
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
