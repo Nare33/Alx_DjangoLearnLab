922f94d81c078e2323f9bc105749159e81c04e7d
Nare33 <ulendaphaho@gmail.com>
2025-03-24 05:30:20 -0700

    User authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,11 +12,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
+        user = User.objects.create_user(**validated_data)
         if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+            user.set_password(password)
+        user.save()
+        return user
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
