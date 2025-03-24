1b5b8fbee1ce8cd37156e38bb6ffaa53091c97c2
Nare33 <ulendaphaho@gmail.com>
2025-03-24 03:04:10 -0700

    User authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -10,5 +11,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
         return user
 
