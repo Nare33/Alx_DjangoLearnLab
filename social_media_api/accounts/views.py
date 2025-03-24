daec0d25c4a985fb354916adfdb7bc0ff73653f3
Nare33 <ulendaphaho@gmail.com>
2025-03-24 06:35:54 -0700

    Posts and comments functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -11,20 +11,37 @@ User = get_user_model()
 
 @api_view(['POST'])
 def follow_user(request, user_id):
+    """
+    Follow a user
+    """
+    if not request.user.is_authenticated:
+        return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+
     user_to_follow = get_object_or_404(User, id=user_id)
+
     if user_to_follow == request.user:
         return Response({'detail': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
     
     request.user.following.add(user_to_follow)
+    
     return Response({'detail': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def unfollow_user(request, user_id):
+    """
+    Unfollow a user
+    """
+    if not request.user.is_authenticated:
+        return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+
     user_to_unfollow = get_object_or_404(User, id=user_id)
+
     if user_to_unfollow == request.user:
         return Response({'detail': 'You cannot unfollow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
     
     request.user.following.remove(user_to_unfollow)
+    
     return Response({'detail': f'You have unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
 
 class UserCreate(generics.CreateAPIView):
