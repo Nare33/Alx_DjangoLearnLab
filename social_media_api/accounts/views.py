7184e3cdb20cf0740f757b57322075a4de214ebb
Nare33 <ulendaphaho@gmail.com>
2025-03-25 05:22:39 -0700

    Follows and feeds

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,12 +1,11 @@
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthentcated
-from django.shortcuts import get_object_or_404
-from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
-from accounts.models import CustomUser
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -29,20 +28,19 @@ def unfollow_user(request, user_id):
     return Response({'message': 'You are not following this user.'}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()  
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class UserRegistrationView(generics.CreateAPIView):
-    queryset = get_user_model().objects.all()
+    queryset = CustomUser.objects.all()  
     serializer_class = UserSerializer
-    
 
 class UserLoginView(generics.GenericAPIView):
     serializer_class = UserSerializer
 
     def post(self, request):
-        serializer = UserSerializer(data = request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.data)
-        return Response (serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
