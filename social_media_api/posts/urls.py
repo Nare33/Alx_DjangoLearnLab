32c6c2e3e199c2dfafc68cb328e0e97e58e43ad6
Nare33 <ulendaphaho@gmail.com>
2025-03-25 06:46:20 -0700

    Notifications and likes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedView, LikeViewSet
+from django.urls import path
+from . import views
 
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
-router.register(r'likes', LikeViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('<int:pk>/like/', views.like_post, name='like_post'),
+    path('<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
+]
