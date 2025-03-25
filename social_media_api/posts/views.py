4ed724eaab028c1d07a96d55264c2e805bcaf179
Nare33 <ulendaphaho@gmail.com>
2025-03-25 04:50:10 -0700

    Follows and feeds

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,61 +1,59 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters, generics
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
-from rest_framework import filters
+from rest_framework import filters, status
 from .serializers import PostSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-class FeedView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PostPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def update(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != request.user:
-            raise PermissionDenied("You cannot edit this post.")
-        return super().update(request, *args, **kwargs)
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
 
-    def destroy(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != request.user:
-            raise PermissionDenied("You cannot delete this post.")
-        return super().destroy(request, *args, **kwargs)
+    def get_queryset(self):
+        return Post.objects.all().order_by('-created_at')
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PostPagination
 
     def perform_create(self, serializer):
-        post = Post.objects.get(id=self.request.data['post'])
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user)
 
-    def update(self, request, *args, **kwargs):
-        comment = self.get_object()
-        if comment.author != request.user:
-            raise PermissionDenied("You cannot edit this comment.")
-        return super().update(request, *args, **kwargs)
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
 
-    def destroy(self, request, *args, **kwargs):
-        comment = self.get_object()
-        if comment.author != request.user:
-            raise PermissionDenied("You cannot delete this comment.")
-        return super().destroy(request, *args, **kwargs)
+    def get_queryset(self):
+        return Comment.objects.all().order_by('-created_at')
 
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PostPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.followers.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
